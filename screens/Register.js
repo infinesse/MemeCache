141aa0b2c70dc9744a97b340e6db5d8c02c7f2fe
Christopher Dupre <meowterspace@ChristophersMBP.domain>
2019-03-22 14:20:07 -0700

    aggregated text input information to alert

--- screens/Register.js
+++ screens/Register.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, TextInput, Button, StyleSheet } from 'react-native'
 
 class Register extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            credentials: {
+                login: "",
+                password: ""
+            }
+        };
+    }
+    updateText(text, field){
+        let newCredentials = Object.assign(this.state.credentials);
+        newCredentials[field] = text;
+        this.setState({
+            credentials: newCredentials
+        });
+    }
+
     register(){
         //send credentials to server
         //if signup success
-            this.props.navigation.navigate('main');
+        alert(JSON.stringify(this.state.credentials));
+            // this.props.navigation.navigate('main');
         //else error message
-    }
+    }  
     render(){
         return (
             <View 
@@ -18,12 +37,12 @@ class Register extends Component {
                     justifyContent: "center",
                     alignItems: "center",
                     backgroundColor: 'rgb(252,61,57)'
-                }}
+                }} 
                     
                     >
             <Text>Register</Text>
-            <TextInput placeholder="Username" style={styles.input}/>
-            <TextInput secureTextEntry placeholder="Password" style={styles.input}/>
+            <TextInput autoCorrect={false} value={this.state.login} onChangeText={text => this.updateText(text, 'login')} placeholder="Username" style={styles.input}/>
+            <TextInput autoCorrect={false} value={this.state.password} onChangeText={text => this.updateText(text, 'password')} secureTextEntry placeholder="Password" style={styles.input}/>
             <Button 
                 onPress={()=>{this.register();}} title="Signup" />
             </View>
