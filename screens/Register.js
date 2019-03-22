2cc9c0b157884740b2055f6303af2270c715a822
Christopher Dupre <meowterspace@ChristophersMBP.domain>
2019-03-22 13:55:26 -0700

    updates secure registration page

--- screens/Register.js
+++ screens/Register.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, TextInput, Button } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Button, StyleSheet } from 'react-native'
 
 class Register extends Component {
     register(){
-        this.props.navigation.navigate('main');
+        //send credentials to server
+        //if signup success
+            this.props.navigation.navigate('main');
+        //else error message
     }
     render(){
         return (
-            <TouchableOpacity 
+            <View 
                 style={{
                     height: 100 + "%", 
                     width: 100 + "%", 
                     flex: 1,
                     justifyContent: "center",
-                    alignItems: "center" }}
-                    onPress={()=>{
-                        this.register();}}
+                    alignItems: "center",
+                    backgroundColor: 'rgb(252,61,57)'
+                }}
+                    
                     >
-            <Text>Register</Text></TouchableOpacity>
+            <Text>Register</Text>
+            <TextInput placeholder="Username" style={styles.input}/>
+            <TextInput secureTextEntry placeholder="Password" style={styles.input}/>
+            <Button 
+                onPress={()=>{this.register();}} title="Signup" />
+            </View>
         );
     }
-} 
+}  
+
+const styles = StyleSheet.create({
+    input: {
+    height: 50,
+    width: 100 + "%",
+    paddingHorizontal: 50,
+    backgroundColor: 'rgb(255,255,255)',
+    marginBottom: 10
+
+    }
+});
+
 export default Register;
