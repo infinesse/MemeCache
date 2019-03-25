b606c01508482de6eb229e9689ae3689896b1654
Christopher Dupre <meowterspace@ChristophersMBP.domain>
2019-03-25 11:22:53 -0700

    addded pull to refresh, found error cant fetch

--- components/container/PostFeed.js
+++ components/container/PostFeed.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import Post from '../presentation/index.js';
 
 class PostFeed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          refreshing: false,
+        };
+      }
+
+      _onRefresh = () => {
+        this.setState({refreshing: true});
+        fetchData().then(() => {
+          this.setState({refreshing: false});
+        });
+      }
 
     _renderPost({item}) {
         return <Post item={item}/>;
@@ -16,6 +29,12 @@ class PostFeed extends Component {
             data={[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]} 
             keyExtractor={this._returnKey}
             renderItem={this._renderPost}
+            refreshControl={
+                <RefreshControl
+                  refreshing={this.state.refreshing}
+                  onRefresh={this._onRefresh}
+                />
+              }
         />;
             
         
